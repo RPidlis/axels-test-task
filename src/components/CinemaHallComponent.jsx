d4a4f77f04627888ea131baf2a3ad869e491f4e3
RPidlis <46480078+RPidlis@users.noreply.github.com>
2021-05-17 17:52:48 +0300

    added the ability to purchase tickets

--- src/components/CinemaHallComponent.jsx
+++ src/components/CinemaHallComponent.jsx
@@ -1,25 +1,3 @@
-// import { Container, Row } from 'react-bootstrap';
-//
-// import { Seat } from '../styled/components/cinemaHall';
-//
-// const CinemaHallComponent = ({ sessionSeats,seats }) => (
-//   <Container className="d-flex flex-column justify-content-around p-0">
-//     <Row className="d-flex justify-content-between pb-2">
-//       {seats.map((item) => (
-//         <Seat
-//           key={item.id}
-//           variant={sessionSeats.includes(item.id) ? 'info' : 'outline-info'}
-//           disabled={sessionSeats.includes(item.id)}
-//         >
-//           {item.id}
-//         </Seat>
-//       ))}
-//     </Row>
-//   </Container>
-// );
-//
-// export default CinemaHallComponent;
-
 import { Container, Row } from 'react-bootstrap';
 
 import { Seat } from '../styled/components/cinemaHall';
