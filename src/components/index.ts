fea4b01309bb344556469009e412eed91a6e7b46
RPidlis <46480078+RPidlis@users.noreply.github.com>
2021-05-27 08:18:50 +0300

    clean_code 1.2.1

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import ModalWindowComponent from './ModalWindowComponent';
 import CinemaHallComponent from './CinemaHallComponent';
 import ScheduleComponent from './ScheduleComponent';
 import SessionComponent from './SessionComponent';
+import TicketsComponent from "./TicketsComponent";
 
 export {
   CinemaHallComponent,
   ScheduleComponent,
   SessionComponent,
-  ModalWindowComponent
+  ModalWindowComponent,
+  TicketsComponent
 };
